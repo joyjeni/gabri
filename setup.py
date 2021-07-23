aabe826d87124c7b74da7838a109d85d5f92f112
Jenisha <joyjeni@users.noreply.github.com>
2021-07-23 22:14:11 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="gabri",
+    version="0.0.2",
+    author="Jenisha Thankaraj",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6.8',
+    install_requires=[
+        "tqdm~=4.48.2",
+        "matplotlib~=3.2.2",
+        "tensorboard~=2.2.0",
+        "albumentations~=0.4.6",
+        "opencv-python~=4.4.0.42",
+        "pytorch-lightning~=1.0.0",
+        "ipympl~=0.5.8"
+    ]
+)
